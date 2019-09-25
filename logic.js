db79f88c0fae8b6952388b7d69012d9b09d41c60
Zion Brown <zabs95@gmail.com>
2019-09-25 13:48:31 -0400

    All the various funtionalities are now accessible via inquirer

--- logic.js
+++ logic.js
@@ -11,99 +11,135 @@ var moment = require('moment');
 
 var file = require('file-system');
 
+var inquirer = require("inquirer");
+
 var spotifyKey = new Spotify(keys.spotify);
 
-var task = process.argv[2]
+// var task = process.argv[2]
 
-var input = []
+// var input = []
 
-input.push(process.argv[3])
+// input.push(process.argv[3])
 
 for (i=4; i< (process.argv.length); i++) {
     input.push(process.argv[i])
     }
 
-// if (task === "do"){
-//     file.readFile('./random.txt', 'utf8', function(err, data){
-//       if (err){ 
-//         return console.log(err);
-//       }
-//       console.log(data)
-//       var dataArr = data.split(',');
-//       console.log(dataArr[0], dataArr[1]);
-//       input = data
-//     });
-// }
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "task",
+        message: "What would you like to search for?",
+        choices: ["band", "music", "movie"]
+      },
 
-    switch (task) {
-  case "band": {
+      {
+        type: "input",
+        name: "input",
+        message: "Search:"
+      },
+    ]).then(function(input) {
 
-var artist = input.join(" ")
+  switch (input.task) {
+      case "band": {
 
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-  function(response) {
-          if ((response.data != "\n{warn=Not found}\n") && (response.data.length > 0)) {
-          console.log("Taria says: " + artist + " is playing at:")
-      for (i=0; i<response.data.length; i++){
-      var venue = response.data[i].venue.name
-      var location = response.data[i].venue.city + ", " + response.data[i].venue.region
-      var date = moment(response.data[i].datetime).format("MM/DD/YYYY")
-      console.log("(" + venue + ") - " + location + " on " + date)
-      }}
-      else{console.log("Taria says: There are no upcoming tours by this artist")}
-  })
-  break;
-}
+    var artist = input.input
 
-  case "music": {
-  var song = '"' + input.join(" ") + '"'
-spotifyKey.search({ type: 'track', query: song, limit: 1 })
-.then(function(response) {
-  var songData = (response.tracks.items[0])
-  var band = (songData.artists[0].name) //Band name
-  var link = (songData.external_urls.spotify) //Link to song
-  var songName = (songData.name) //Song name
-  var albumName = (songData.album.name) //album name
-  console.log("Taria says: " + songName + ", released by: " + band)
-  console.log("Part of the " + albumName + " album")
-  console.log("link:", link)
-})
-.catch(function(err) {
-  console.log(err)
-})
-break;
-}
-
-  case "movie": { // Movie search
-//takes the input and joins all instances in the array with a hyphen
-var movieName = input.join("-")
-//builds the url
-var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=ded7cf4";
-//searches for the movie
-axios.get(movieUrl).then(
-    function(response) {
-//console.log(response)
-    console.log("Taria says: " + response.data.Title);
-    console.log("----Info----")
-    console.log("Was released in: " + response.data.Year);
-    console.log("Produced by: " + response.data.Country)
-    console.log("Languages avialable: " + response.data.Language)
-    console.log("Cast: " + response.data.Actors)
-    console.log("----Ratings----")
-    console.log(response.data.Ratings[0].Source + " rating: " + response.data.Ratings[0].Value)
-    console.log(response.data.Ratings[1].Source + " rating: " + response.data.Ratings[1].Value)
-    console.log("----Plot----")
-    console.log(response.data.Plot)
-
-    // * Plot of the movie.
-    // * Actors in the movie.
-
-})
-break;
-}
-
-  case undefined: { // If none of the available calls are selected
-console.log("Taria says: I'm sorry, I didn't catch that.")
-break;
-}
+    if (artist.trim() === "") {
+      console.log("Taria says: No artist selected")
+    }else {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+      function(response) {
+              if ((response.data != "\n{warn=Not found}\n") && (response.data.length > 0)) {
+              console.log("Taria says: " + artist + " is playing at:")
+          for (i=0; i<response.data.length; i++){
+          var venue = response.data[i].venue.name
+          var location = response.data[i].venue.city + ", " + response.data[i].venue.region
+          var date = moment(response.data[i].datetime).format("MM/DD/YYYY")
+          console.log("(" + venue + ") - " + location + " on " + date)
+          }}
+          else{console.log("Taria says: There are no upcoming tours by this artist")}
+      })}
+      break;
     }
+
+      case "music": {
+
+      var song = input.input
+
+      if (song.trim() === "") {
+        console.log('Taria says: Default song: The "Sign"')
+        song = "The Sign"
+      }
+    spotifyKey.search({ type: 'track', query: song, limit: 1 })
+
+    .then(function(response) {
+      var songData = (response.tracks.items[0])
+      var band = (songData.artists[0].name) //Band name
+      var link = (songData.external_urls.spotify) //Link to song
+      var songName = (songData.name) //Song name
+      var albumName = (songData.album.name) //album name
+      console.log("Taria says: " + songName + ", released by: " + band)
+      console.log("Part of the " + albumName + " album")
+      console.log("link:", link)
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+    break;
+    }
+
+      case "movie": { // Movie search
+    //takes the input and joins all instances in the array with a hyphen
+    var movieName = input.input
+
+    if (movieName.trim() === "") {
+      console.log('Taria says: No movie selected')
+    } else {
+    //builds the url
+    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=ded7cf4";
+    //searches for the movie
+    axios.get(movieUrl).then(
+        function(response) {
+var info = response.data
+var title = info.Title
+var year = info.Year
+var country = info.Country
+var lang = info.Language
+var cast = info.Actors
+var rating1 = info.Ratings[0]
+var rating2 = info.Ratings[1]
+var plot = info.Plot
+        console.log("Taria says: " + title);
+        console.log("----Info----")
+        console.log("Was released in: " + year);
+        console.log("Produced by: " + country)
+        console.log("Languages avialable: " + lang)
+        console.log("Cast: " + cast)
+        console.log("----Ratings----")
+        console.log(rating1.Source + " rating: " + rating1.Value)
+        console.log(rating2.Source + " rating: " + rating2.Value)
+        console.log("----Plot----")
+        console.log(plot)
+
+    })}
+    break;
+    }
+
+    case "do": {
+      file.readFile('./random.txt', 'utf8', function(err, data){
+        if (err){ 
+          return console.log(err);
+        }
+        var dataArr = data.split(',');
+        console.log(dataArr[0], dataArr[1]);
+      });
+    }
+
+      default: { // If none of the available calls are selected
+    console.log("Taria says: I'm sorry, I didn't catch that.")
+    break;
+    }
+  }
+  
+});
