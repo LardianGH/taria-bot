ee6cba0410a5d480d5472a25618be641902fe09f
Zion Brown <zabs95@gmail.com>
2019-09-25 15:57:08 -0400

    Taria can now take orders from an external text file

--- logic.js
+++ logic.js
@@ -15,92 +15,56 @@ var inquirer = require("inquirer");
 
 var spotifyKey = new Spotify(keys.spotify);
 
-// var task = process.argv[2]
+//all the functions
+var band = function(artist) {
+  if (artist.trim() === "") {
+    console.log("Taria says: No artist selected")
+  }else {
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+    function(response) {
+            if ((response.data != "\n{warn=Not found}\n") && (response.data.length > 0)) {
+            console.log("Taria says: " + artist + " is playing at:")
+        for (i=0; i<response.data.length; i++){
+        var venue = response.data[i].venue.name
+        var location = response.data[i].venue.city + ", " + response.data[i].venue.region
+        var date = moment(response.data[i].datetime).format("MM/DD/YYYY")
+        console.log("(" + venue + ") - " + location + " on " + date)
+        }}
+        else{console.log("Taria says: There are no upcoming tours by this artist")}
+    })}
+}
 
-// var input = []
+var music = function(song) {
+  if (song.trim() === "") {
+    console.log('Taria says: Default song: The "Sign"')
+    song = "The Sign"
+  }
+spotifyKey.search({ type: 'track', query: song, limit: 1 })
 
-// input.push(process.argv[3])
+.then(function(response) {
+  var songData = (response.tracks.items[0])
+  var band = (songData.artists[0].name) //Band name
+  var link = (songData.external_urls.spotify) //Link to song
+  var songName = (songData.name) //Song name
+  var albumName = (songData.album.name) //album name
+  console.log("Taria says: " + songName + ", released by: " + band)
+  console.log("Part of the " + albumName + " album")
+  console.log("link:", link)
+})
+.catch(function(err) {
+  console.log(err)
+})
+}
 
-for (i=4; i< (process.argv.length); i++) {
-    input.push(process.argv[i])
-    }
-
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "task",
-        message: "What would you like to search for?",
-        choices: ["band", "music", "movie"]
-      },
-
-      {
-        type: "input",
-        name: "input",
-        message: "Search:"
-      },
-    ]).then(function(input) {
-
-  switch (input.task) {
-      case "band": {
-
-    var artist = input.input
-
-    if (artist.trim() === "") {
-      console.log("Taria says: No artist selected")
-    }else {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-      function(response) {
-              if ((response.data != "\n{warn=Not found}\n") && (response.data.length > 0)) {
-              console.log("Taria says: " + artist + " is playing at:")
-          for (i=0; i<response.data.length; i++){
-          var venue = response.data[i].venue.name
-          var location = response.data[i].venue.city + ", " + response.data[i].venue.region
-          var date = moment(response.data[i].datetime).format("MM/DD/YYYY")
-          console.log("(" + venue + ") - " + location + " on " + date)
-          }}
-          else{console.log("Taria says: There are no upcoming tours by this artist")}
-      })}
-      break;
-    }
-
-      case "music": {
-
-      var song = input.input
-
-      if (song.trim() === "") {
-        console.log('Taria says: Default song: The "Sign"')
-        song = "The Sign"
-      }
-    spotifyKey.search({ type: 'track', query: song, limit: 1 })
-
-    .then(function(response) {
-      var songData = (response.tracks.items[0])
-      var band = (songData.artists[0].name) //Band name
-      var link = (songData.external_urls.spotify) //Link to song
-      var songName = (songData.name) //Song name
-      var albumName = (songData.album.name) //album name
-      console.log("Taria says: " + songName + ", released by: " + band)
-      console.log("Part of the " + albumName + " album")
-      console.log("link:", link)
-    })
-    .catch(function(err) {
-      console.log(err)
-    })
-    break;
-    }
-
-      case "movie": { // Movie search
-    //takes the input and joins all instances in the array with a hyphen
-    var movieName = input.input
-
-    if (movieName.trim() === "") {
-      console.log('Taria says: No movie selected')
-    } else {
-    //builds the url
-    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=ded7cf4";
-    //searches for the movie
-    axios.get(movieUrl).then(
-        function(response) {
+var movie = function(movieName) {
+  if (movieName.trim() === "") {
+    console.log('Taria says: No movie selected')
+  } else {
+  //builds the url
+  var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=ded7cf4";
+  //searches for the movie
+  axios.get(movieUrl).then(
+      function(response) { //Need some way to loop through all these variables to catch any undefined values.
 var info = response.data
 var title = info.Title
 var year = info.Year
@@ -110,19 +74,60 @@ var cast = info.Actors
 var rating1 = info.Ratings[0]
 var rating2 = info.Ratings[1]
 var plot = info.Plot
-        console.log("Taria says: " + title);
-        console.log("----Info----")
-        console.log("Was released in: " + year);
-        console.log("Produced by: " + country)
-        console.log("Languages avialable: " + lang)
-        console.log("Cast: " + cast)
-        console.log("----Ratings----")
-        console.log(rating1.Source + " rating: " + rating1.Value)
-        console.log(rating2.Source + " rating: " + rating2.Value)
-        console.log("----Plot----")
-        console.log(plot)
+      console.log("Taria says: " + title);
+      console.log("----Info----")
+      console.log("Was released in: " + year);
+      console.log("Produced by: " + country)
+      console.log("Languages avialable: " + lang)
+      console.log("Cast: " + cast)
+      console.log("----Ratings----")
+      console.log(rating1.Source + " rating: " + rating1.Value)
+      console.log(rating2.Source + " rating: " + rating2.Value)
+      console.log("----Plot----")
+      console.log(plot)
 
-    })}
+  })}
+}
+//end all the functions
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "task",
+        message: "What would you like to search for?",
+        choices: ["band", "music", "movie", "do"]
+      },
+      {
+        type: "input",
+        name: "input",
+        message: "Search:"
+      },
+    ]).then(function(input) {
+//switch case
+  switch (input.task) {
+      case "band": {
+
+    var artist = input.input
+
+    band(artist)
+
+    break;
+    }
+
+      case "music": {
+
+    var song = input.input
+
+    music(song)
+      
+    break;
+    }
+
+      case "movie": { // Movie search
+    //takes the input and joins all instances in the array with a hyphen
+    var movieName = input.input
+
+    movie(movieName)
+   
     break;
     }
 
@@ -132,7 +137,35 @@ var plot = info.Plot
           return console.log(err);
         }
         var dataArr = data.split(',');
-        console.log(dataArr[0], dataArr[1]);
+
+        var doInstructions = dataArr[1]
+        switch (dataArr[0]) {
+          case "band": {
+    
+        var artist = doInstructions
+    
+        band(artist)
+    
+        break;
+        }
+    
+          case "music": {
+    
+        var song = doInstructions
+    
+        music(song)
+          
+        break;
+        }
+    
+          case "movie": { // Movie search
+        //takes the input and joins all instances in the array with a hyphen
+        var movieName = doInstructions
+    
+        movie(movieName)
+       
+        break;
+        }}
       });
     }
 
@@ -141,5 +174,5 @@ var plot = info.Plot
     break;
     }
   }
-  
+//end switch case
 });
